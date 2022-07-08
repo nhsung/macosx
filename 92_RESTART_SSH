4747f21e0dbaff4e74f3b1edec272060a82757e8
nhsung <nhsung@gmail.com>
2022-07-08 14:36:30 +0900

    1

---
+++ 92_RESTART_SSH
@@ -0,0 +1,2 @@
+sudo launchctl unload /System/Library/LaunchDaemons/ssh.plist
+sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist
---
+++ backup.sh
@@ -0,0 +1,6 @@
+cd ~/readme/99_MACOS
+sudo scp -rp ./* ~/NFS_DATA/ReadMe/99_MACOS/
+
+echo "ls ~/NFS_DATA/ReadMe/99_MACOS"
+echo
+ls ~/NFS_DATA/ReadMe/99_MACOS
